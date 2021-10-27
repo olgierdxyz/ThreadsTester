0c7d7a64abea83893ac12afb16b51247181c3053
Olgierd <olgierdxyz@gmail.com>
2021-10-27 11:50:36 +0200

    Improved version

--- src/main/java/me/noip/threads/CreateThreadsCount.java
+++ src/main/java/me/noip/threads/CreateThreadsCount.java
@@ -6,22 +6,17 @@ import java.util.logging.Logger;
 public class CreateThreadsCount {
 
     public static int SLEEP;
+    public static int THREADS;
 
     public static void main(String[] args) {
         SLEEP = Integer.parseInt(args[0]);
-        for (int i = 1; i <= Integer.parseInt(args[1]); i++) {
-            System.out.println("Let's create a new thread [" + i + "]...");
+        THREADS = Integer.parseInt(args[1]);
+        System.out.println("# The program started with arguments: sleep=" + SLEEP + ", threads=" + THREADS + ".");       
+        for (int i = 1; i <= THREADS; i++) {
             Hello thread = new Hello();
             thread.start();
         }
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(CreateThreadsCount.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println("Active threads in the current thread group:" + Thread.activeCount());
     }
-
 }
 
 class Hello extends Thread {
